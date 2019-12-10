57a3cb52a4b90c8c2eafcba04a674180e0510081
Luis Villas <shusten@hotmail.com>
2019-12-10 01:04:14 -0300

    Terminado classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -4,6 +4,36 @@ public class Pessoa {
     private String Sexo;
 
     public void fazerAniver() {
+        this.idade = idade + 1;
+    }
 
+    public Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        Sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
     }
 }
